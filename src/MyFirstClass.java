079a50f62b54eedcbef4b5d3e66046dc8cf148fb
Tyler Pickard <100139376@lubbockisd.net>
2022-08-31 10:13:58 -0500

    Initial commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,8 @@
+public class MyFirstClass {
+    public static void main(String[] args) {
+        System.out.print("Hello!");
+        System.out.println(" " + "This is a silent place, huh?");
+        System.out.println("I mean, I'm the only one talking.");
+        System.out.println("But hey, I'm just a program!");
+    }
+}
