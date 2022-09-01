5aee78fa894578ea3ca9edb02b955ad48bc724cb
MangledMan <Themangle906@gmail.com>
2022-09-01 09:20:34 -0500

    Added 2 new println messages

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -4,5 +4,7 @@ public class MyFirstClass {
         System.out.println(" " + "This is a silent place, huh?");
         System.out.println("I mean, I'm the only one talking.");
         System.out.println("But hey, I'm just a program!");
+        System.out.println("But I'm kinda sad :(");
+        System.out.println("Don't worry, I'm actually happy :D");
     }
 }
